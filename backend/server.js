effa9dbf19e0323f08e1f6c925b877f894f8e893
eduardofortes10 <dudupeed25@gmail.com>
2025-08-02 19:38:24 -0300

    fix: usa porta dinâmica para o Render

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,8 @@ process.on('unhandledRejection', (err) => {
 });
 
 // Inicialização
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
