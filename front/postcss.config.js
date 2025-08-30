0dadc00c739448229ad3a796bc11b6d4c0443e70
eduardofortes10 <dudupeed25@gmail.com>
2025-08-30 19:53:10 -0300

    teste 0002

--- front/postcss.config.js
+++ front/postcss.config.js
@@ -1,20 +1,7 @@
+// postcss.config.js
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
-// tailwind.config.js
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  safelist: [
-    "from-[var(--grad-from)]",
-    "via-[var(--grad-via)]",
-    "to-[var(--grad-to)]",
-    "text-brand",
-    "text-brand-strong",
-    "bg-brand",
-  ],
-  theme: { extend: {} },
-  plugins: [],
 };
